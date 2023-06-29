0e5392534fab14c7fa766a9fe9efd86bcca9a9c5
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-06-29 17:45:57 +0800

    battery: sec_battery: Implement battery charging_enabled node
    * Support battery charging bypass for any percentages higher than 5%.
    
    Change-Id: I73bd58c435adf53e9c9c76b6d9d44ac9d65ea674

--- drivers/battery/common/sec_battery.h
+++ drivers/battery/common/sec_battery.h
@@ -1134,6 +1134,10 @@ struct sec_battery_info {
 	bool vbat_adc_open;
 #endif
 
+	/* charging disabler */
+	bool charging_enabled;
+	bool charging_suspended;
+
 	/* usb suspend */
 	int prev_usb_conf;
 
