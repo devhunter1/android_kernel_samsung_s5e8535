98799233e85bfe94857e186d429af71940c3d343
devhunter1 <anubhabkundu2@gmail.com>
2025-04-01 11:19:38 +0530

    build.sh: show if the target device is not set

--- build.sh
+++ build.sh
@@ -5,4 +5,11 @@ if [ ! -d "$pwd/kernel_build" ] || [ -z "$(ls -A $pwd/kernel_build)" ]; then
     git submodule update --init
 fi
 
+if [ -z "$DEVICE" ]; then
+    echo "Error: Target device is not set."
+    echo "You can set it by: export DEVICE=*"
+    echo "exiting.."
+    exit 1
+fi
+
 ./kernel_build/build.sh "$(pwd)" || exit 1
