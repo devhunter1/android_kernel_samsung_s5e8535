458133366a7eb7129c3d1b984fa911b9846bb563
devhunter1 <anubhabkundu2@gmail.com>
2025-04-01 11:09:37 +0530

    build.sh: clone submodule if kernel_build dir empty or missing

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ ! -d "$pwd/kernel_build" ] || [ -z "$(ls -A $pwd/kernel_build)" ]; then
+    echo "kernel_build directory is missing or empty. cloning from submodule"
+    git submodule update --init
+fi
+
 ./kernel_build/build.sh "$(pwd)" || exit 1
