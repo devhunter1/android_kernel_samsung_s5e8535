Linux4/android_kernel_samsung_gta4xls

drivers: net: wireless: scsc: Don't delete or deactivate AP interface in slsi_del_station
* This function is meant for cleaning up going from STA to AP mode
* On latest 18.1 builds deleting/deactivating AP interface causes
  hotspot not to work, hostapd starts up and system says hotspot
  is on, but due to a kernel error (vif type isn't set to AP anymore)
  SSID isn't actually broadcasted and clients can't find nor connect
  to the hotspot
* So let's remove this as removing it doesn't seem to have any
  negative consequences but fixes hotspot, also disable an
  ugly warning caused by this which isn't actually a problem

Change-Id: I5a656fd38697b9be09ecdc5f344bc343458aeaaf

drivers/net/wireless/scsc/cfg80211_ops.c
@@ -173,7 +173,7 @@ int slsi_change_virtual_intf(struct wiphy *wiphy,
 
 	SLSI_NET_DBG1(dev, SLSI_CFG80211, "type:%u, iftype:%d\n", type, ndev_vif->iftype);
 
-	if (WARN_ON(ndev_vif->activated)) {
+	if (ndev_vif->vif_type != FAPI_VIFTYPE_AP && WARN_ON(ndev_vif->activated)) {
 		r = -EINVAL;
 		goto exit;
 	}
@@ -1859,12 +1859,7 @@ int slsi_del_station(struct wiphy *wiphy, struct net_device *dev,
 		slsi_clear_cached_ies(&ndev_vif->ap.cache_wpa_ie, &ndev_vif->ap.wpa_ie_len);
 		slsi_clear_cached_ies(&ndev_vif->ap.cache_wmm_ie, &ndev_vif->ap.wmm_ie_len);
 
-		netif_carrier_off(dev);
-
 		/* All STA related packets and info should already have been flushed */
-		if (slsi_mlme_del_vif(sdev, dev) != 0)
-			SLSI_NET_ERR(dev, "slsi_mlme_del_vif failed\n");
-		slsi_vif_deactivated(sdev, dev);
 		ndev_vif->ipaddress = cpu_to_be32(0);
 
 		if (ndev_vif->ap.p2p_gc_keys_set) {

devhunter1/android_kernel_samsung_s5e8535

drivers/net/wireless/scsc/cfg80211_ops.c
@@ -188,7 +188,7 @@ int slsi_change_virtual_intf(struct wiphy *wiphy,
 
 	SLSI_NET_DBG1(dev, SLSI_CFG80211, "type:%u, iftype:%d\n", type, ndev_vif->iftype);
 
-	if (WLBT_WARN_ON(ndev_vif->activated)) {
+	if (ndev_vif->vif_type != FAPI_VIFTYPE_AP && WLBT_WARN_ON(ndev_vif->activated)) {
 		r = -EINVAL;
 		goto exit;
 	}
@@ -1960,12 +1960,7 @@ int slsi_del_station(struct wiphy *wiphy, struct net_device *dev,
 		slsi_clear_cached_ies(&ndev_vif->ap.cache_wpa_ie, &ndev_vif->ap.wpa_ie_len);
 		slsi_clear_cached_ies(&ndev_vif->ap.cache_wmm_ie, &ndev_vif->ap.wmm_ie_len);
 
-		netif_dormant_on(dev);
-
 		/* All STA related packets and info should already have been flushed */
-		if (slsi_mlme_del_vif(sdev, dev) != 0)
-			SLSI_NET_ERR(dev, "slsi_mlme_del_vif failed\n");
-		slsi_vif_deactivated(sdev, dev);
 		ndev_vif->ipaddress = cpu_to_be32(0);
 
 		if (ndev_vif->ap.p2p_gc_keys_set) {