f4d75e9e267a843f486a97ac4e5fc6653716301c
devhunter1 <anubhabkundu2@gmail.com>
2026-05-15 10:59:25 +0530

    usb: host: Use xhci_link_chain_quirk() helper function
    - see d16a2857ad176f6f48c1bfbe20fed0e4784df80d
    
    Change-Id: I63db40a60e31fd5a63bc13d9afec081b783367ce

--- drivers/usb/host/xhci-exynos-audio.c
+++ drivers/usb/host/xhci-exynos-audio.c
@@ -598,7 +598,7 @@ static int xhci_alloc_segments_for_ring_uram(struct xhci_hcd *xhci,
 	bool chain_links;
 
 	/* Set chain bit for 0.95 hosts, and for isoc rings on AMD 0.96 host */
-	chain_links = ! !(xhci_link_trb_quirk(xhci) || (type == TYPE_ISOC && (xhci->quirks & XHCI_AMD_0x96_HOST)));
+	chain_links = xhci_link_chain_quirk(xhci, type);
 
 	if (type == TYPE_ISOC) {
 		prev = xhci_segment_alloc_uram_ep(xhci, cycle_state, max_packet, flags, 0, endpoint_type);
